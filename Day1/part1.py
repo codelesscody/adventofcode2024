42192d933c26ddbbbef1c019f283733628ea19ca
cody sparks <cody@eightfoldroute.com>
2024-12-01 10:38:25 -0600

    part1 complete

--- Day1/part1.py
+++ Day1/part1.py
@@ -12,11 +12,24 @@
 
 # 0: initiialize variables
 distance = 0
+list1 = []
+list2 = []
 
 # 1: open file, read each list into separate arrays, close file
+with open("Day1/input_part1") as file:
+    for line in file:
+        list1.append(line.split(None)[0])
+        list2.append(line.split(None)[1])
 
 # 2: sort each array
+list1.sort()
+list2.sort()
 
 # 3: pop a pair from each list, compare difference (abs(val1-val2)), and add to distance
 
+pairs = zip(list1, list2)
+for x, y in pairs:
+    distance += abs(int(x) - int(y))
+
 # 4. output result
+print(distance)
